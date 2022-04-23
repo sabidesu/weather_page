d512657721a3fd3ccef53250ce5a42157e9dc633
julian donald <zentixzaneic@gmail.com>
2022-04-23 13:41:30 -0400

    insert search bar

---
+++ src/InputBar.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Search from "./Search.js";
+
+const InputBar = (props) => {
+	return (
+		<Search />
+	)
+}
+
+export default InputBar;
---
+++ src/Search.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Search = (props) => {
+	return (
+		<>
+			<p>insert your location below</p>
+			<input type="text" title="please insert location" name="location" />
+			<button>go</button>
+		</>
+	);
+};
+
+export default Search;
