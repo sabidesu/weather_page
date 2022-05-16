1d17d5abbfd6a932a754573cc0ed02bd71b8f4d0
julian donald <zentixzaneic@gmail.com>
2022-05-16 17:30:57 -0400

    show news

--- src/components/News.js
+++ src/components/News.js
@@ -1,12 +1,35 @@
-import { responsiveProperty } from '@mui/material/styles/cssUtils';
 import Typography from '@mui/material/Typography';
+import Grid from '@mui/material/Grid';
+import Button from '@mui/material/Button';
 
 const News = props => {
 	return (
 		<>
 			<Typography variant="h4">yesterday's top news</Typography>
+			{props.news.results.slice(0, 5).map((info) => <Article info={info} key={info.abstract} />)}
 		</>
 	);
 };
+
+const Article = props => {
+	const hasImage = props.info.media.length !== 0;
+	return (
+		<>
+			<Grid container padding={2}>
+				{hasImage ? 
+				<Grid item xs={4}>
+					<img src={props.info.media[0]["media-metadata"][1].url} alt={props.info.media[0].copyright} /> 
+				</Grid>
+				: null}
+				<Grid item xs={hasImage ? 8 : 12}>
+					<Typography variant="h6">{props.info.title}</Typography>
+					<Typography variant="subtitle1">{props.info.byline}</Typography>
+					<Typography variant="body1">{props.info.abstract}</Typography>
+					<Button variant="text" href={props.info.url}>Read More</Button>
+				</Grid>
+			</Grid>
+		</>
+	);	
+}
 
 export default News;
