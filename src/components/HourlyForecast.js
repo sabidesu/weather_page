7d16ab5352102a28ba85227187451dd480f14a42
julian donald <zentixzaneic@gmail.com>
2022-05-15 22:43:38 -0400

    display some info in 'tomorrow'

--- src/components/HourlyForecast.js
+++ src/components/HourlyForecast.js
@@ -3,7 +3,23 @@ import Typography from '@mui/material/Typography';
 const HourlyForecast = props => {
 	return (
 		<>
-			<Typography variant="h4">hourly</Typography>
+			<Typography variant="h4">tomorrow</Typography>
+			{props.weather.filter(hour => {
+				const time = new Date(hour.dt * 1000);
+				return time.getDate() !== new Date(Date.now()).getDate();
+			})
+				.slice(7, 20)
+				.map((weatherInfo) => <Hour weather={weatherInfo} key={weatherInfo.dt}/>)}
+		</>
+	);
+}
+
+const Hour = props => {
+	const date = new Date(props.weather.dt);
+	const time = date.getHours() + ":" + date.getMinutes();
+	return (
+		<>
+			<Typography variant="h6">{time}</Typography>
 		</>
 	);
 }
