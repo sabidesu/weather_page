ea55f4a4ef32faaa6a92320dfe3d3a451419a2ef
julian donald <zentixzaneic@gmail.com>
2022-05-16 18:49:46 -0400

    rearrange daily section

--- src/components/DailyForecast.js
+++ src/components/DailyForecast.js
@@ -5,15 +5,28 @@ const DailyForecast = props => {
 	return (
 		<>
 			<Typography variant="h4">daily</Typography>
-			<Grid container spacing={1}>
-				{props.weather.slice(1).map((weatherInfo) => {
-					return (
-						<Grid item xs={1} key={weatherInfo.dt}>
-							<Day weather={weatherInfo}/>
-						</Grid>
-					)
-				})}
+			<Grid container spacing={2}>
+				<Grid item xs={2}>
+
+				</Grid>
+				<Grid item xs={2}>
+					<Typography variant="overline">date</Typography>
+				</Grid>
+				<Grid item xs={2}>
+					<Typography variant="overline">weather</Typography>
+				</Grid>
+				<Grid item xs={2}>
+					<Typography variant="overline">high</Typography>
+				</Grid>
+				<Grid item xs={2}>
+					<Typography variant="overline">low</Typography>
+				</Grid>
 			</Grid>
+			{props.weather.slice(1).map((weatherInfo) => {
+				return (
+					<Day weather={weatherInfo}/>
+				)
+			})}
 		</>
 	);
 };
@@ -25,10 +38,23 @@ const Day = props => {
 
 	return (
 		<>
-			<Typography variant="h6">{short}</Typography>
-			<img src={icon_src} alt={props.weather.weather[0].description} />
-			<Typography variant="body1">{Math.round(props.weather.temp.max)}</Typography>
-			<Typography variant="body1">{Math.round(props.weather.temp.min)}</Typography>
+		 	<Grid container spacing={2}>
+			 	<Grid item xs={2}>
+
+				</Grid>
+			 	<Grid item xs={2}>
+					<Typography variant="subtitle1">{short}</Typography>
+				</Grid>
+				<Grid item xs={2}>
+					<img src={icon_src} alt={props.weather.weather[0].description} />
+				</Grid>
+				<Grid item xs={2}>
+					<Typography variant="h5">{Math.round(props.weather.temp.max)}</Typography>
+				</Grid>
+				<Grid item xs={2}>
+					<Typography variant="h5">{Math.round(props.weather.temp.min)}</Typography>
+				</Grid>
+			</Grid>
 		</>
 	);
 }
